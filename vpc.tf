091bfee02e5199bca7953363c108e7206bfa868b
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-08 18:33:16 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,7 @@ resource "aws_vpc" "main" {
   }
 }
 #Creates Public Subnet 
+resource "aws_subnet" "Public02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
