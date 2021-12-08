a154b1b93fdbce662ba9a7263b79157f88296852
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-08 18:34:25 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,12 +8,12 @@ resource "aws_vpc" "main" {
   }
 }
 #Creates Public Subnet 
-resource "aws_subnet" "Public02" {
+resource "aws_subnet" "Public01" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "Public02"
+    Name = "Public01"
   map_public_ip_on_launch = true
   }
 }
