dee66318454b17f4160bccb2db7639567fff7133
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-08 18:33:51 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -12,3 +12,8 @@ resource "aws_subnet" "Public02" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  tags = {
+    Name = "Public02"
+  map_public_ip_on_launch = true
+  }
+}
