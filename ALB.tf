40adc7707659c56226e97ded5ce03e27627896fd
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-16 12:32:41 -0500

    Update ALB.tf

--- ALB.tf
+++ ALB.tf
@@ -35,15 +35,15 @@ resource "aws_security_group" "allow_80" {
   }
 }
 
-resource "aws_alb_target_group" "tar_EC2" {
-  name     = "tar_EC2"
+resource "aws_alb_target_group" "tarEC2" {
+  name     = "tarEC2"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 }
 
 resource "aws_lb_target_group_attachment" "Attach_EC2" {
-  target_group_arn = aws_alb_target_group.tar_EC2.arn
+  target_group_arn = aws_alb_target_group.tarEC2.arn
   target_id        = aws_instance.Deploy9_EC2.id
   port             = 80
 }
