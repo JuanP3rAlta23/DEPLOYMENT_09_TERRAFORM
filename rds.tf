983a80fe835b387e863f08af48e31d65e430e10f
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-17 11:38:45 -0500

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -11,9 +11,28 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name   = aws_db_subnet_group.internals-db.id
 }
 
+
+resource "aws_security_group" "database-sg" {
+  name        = "deploy9-db-sg"
+  description = "Take traffic from ec2"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP from EC2"
+    from_port       = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.allow_80_EC2.id]
+    to_port         = 5432
+  }
+
+  tags = {
+    "Name" = "deployment 9 security group for database "
+  }
+}
+
 resource "aws_db_subnet_group" "internals-db" {
   name       = "internals-db"
-  subnet_ids = [aws_subnet.internal1.id, aws_subnet.internal2.id]
+  subnet_ids = [aws_subnet.Internal01.id, aws_subnet.Internal02.id]
 
   tags = {
     Name = "DB-subnet-group"
