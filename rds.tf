3f27dbf0c8f331efa2a4e296679091864b7cdbcc
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-17 12:46:59 -0500

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,13 +1,14 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "postgres"
-  engine_version       = "9.6"
-  instance_class       = "db.t2.micro"
-  name                 = "Deploy9db"
-  multi_az             = true
-  username             = "Juan"
-  password             = "DaDBPassword"
-  skip_final_snapshot  = true
+resource "aws_db_instance" "deploy9db" {
+  allocated_storage      = 10
+  engine                 = "postgres"
+  engine_version         = "9.6"
+  instance_class         = "db.t2.micro"
+  name                   = "Deploy9db"
+  multi_az               = true
+  username               = "Juan"
+  password               = "DaDBPassword"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
   db_subnet_group_name   = aws_db_subnet_group.internals-db.id
 }
 
