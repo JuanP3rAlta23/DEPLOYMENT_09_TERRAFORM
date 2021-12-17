af660f14c34b4ae8ad04130275829f76cfe123ec
Juan Peralta <88753706+JuanP3rAlta23@users.noreply.github.com>
2021-12-17 09:50:18 -0500

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,9 +1,10 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "postgres"
-  engine_version         = "9.6"
+  engine_version       = "9.6"
   instance_class       = "db.t2.micro"
   name                 = "mydb"
+  multi_az             = true
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
